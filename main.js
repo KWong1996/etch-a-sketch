3e93cca33b8f232a1baade0297328bae48c3dd8b
Kevin Wong <1996kevinwong@gmail.com>
2018-01-04 22:55:35 -0500

    Working prototype on a 16x16 grid

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
 var wrapper = document.querySelector('.wrapper');
 
+
 //Initial grid size
 var n = 16;
 
+function getSize(){
+    var prompt = prompt("Enter new grid size (NxN)");
+}
+
 function createGrid(n){
 
-    for(var i = 0; i < n; i++){
+    wrapper.style.gridTemplateColumns = "repeat(n, 1fr)";
+    wrapper.style.gridTemplateRows = "repeat(n, 1fr)"
+
+    for(var i = 0; i < (n*n); i++){
 
         let block = document.createElement('div');
         block.classList.add('block');
+        block.onmouseover = function(){
+            block.classList.add('filled');
+        }
         wrapper.appendChild(block);
         
     }
 }
+
+
